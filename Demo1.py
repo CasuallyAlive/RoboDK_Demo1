c167af452dda3498f1e48ed1c9cb067ef8606d3d
CasuallyAlive <Jordy.larrearodriguez@gmail.com>
2023-02-20 13:16:54 -0700

    Demo for offline programming ready.

---
+++ Demo1.py
@@ -0,0 +1,27 @@
+# Type help("robodk.robolink") or help("robodk.robomath") for more information
+# Press F5 to run the script
+# Documentation: https://robodk.com/doc/en/RoboDK-API.html
+# Reference:     https://robodk.com/doc/en/PythonAPI/robodk.html
+# Note: It is not required to keep a copy of this file, your Python script is saved with your RDK project
+
+from robodk.robolink import *  # API to communicate with RoboDK
+from robodk.robomath import *  # basic matrix operations
+# Link to RoboDK
+# RDK = Robolink()
+RDK = Robolink()
+
+# Notify user:
+print('To edit this program:\nright click on the Python program, then, select "Edit Python script"')
+
+# Program example:
+item = RDK.Item('base')
+if item.Valid():
+    print('Item selected: ' + item.Name())
+    print('Item posistion: ' + repr(item.Pose()))
+
+print('Items in the station:')
+itemlist = RDK.ItemList()
+robot = RDK.ItemUserPick('Select a robot', ITEM_TYPE_ROBOT)
+print(itemlist)
+
+robot.MoveJ([0.000, 37, 0.000, -0.000, 0.000, -0.000])
